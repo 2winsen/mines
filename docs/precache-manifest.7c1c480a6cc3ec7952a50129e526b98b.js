ac4a795ac9337cfddd44da03ad8a11998c6b1f6e
2winsen <vitalijs.shakels@gmail.com>
2020-02-02 18:14:41 +0200

    favicon and docs

---
+++ docs/precache-manifest.7c1c480a6cc3ec7952a50129e526b98b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b8986234da7f26aae6844bb1bcf9bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f59272d3225233a29aec",
+    "url": "./static/css/main.d7472716.chunk.css"
+  },
+  {
+    "revision": "d83f569c8c4a28008aff",
+    "url": "./static/js/2.12618ca4.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "./static/js/2.12618ca4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f59272d3225233a29aec",
+    "url": "./static/js/main.1a42c22b.chunk.js"
+  },
+  {
+    "revision": "c93fa102b7b4bf64d8b1",
+    "url": "./static/js/runtime-main.123387cc.js"
+  },
+  {
+    "revision": "fc12b71f9bc8e16d096fdd01bf4efceb",
+    "url": "./static/media/flag.fc12b71f.svg"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "./static/media/hamburger.86595150.svg"
+  },
+  {
+    "revision": "f459666c54dbcfc5b8ed1889a251fd65",
+    "url": "./static/media/mine-wrong.f459666c.svg"
+  },
+  {
+    "revision": "e3d5f918ba98f50c53b83259dea191b0",
+    "url": "./static/media/mine.e3d5f918.svg"
+  },
+  {
+    "revision": "b3545baca1ddec400cd7a161058891d3",
+    "url": "./static/media/smile-lost.b3545bac.svg"
+  },
+  {
+    "revision": "aadfd8ca07fefec88cc26de9ce4a5b0c",
+    "url": "./static/media/smile-won.aadfd8ca.svg"
+  },
+  {
+    "revision": "dfb86436bb23e914129a4eee12a7b850",
+    "url": "./static/media/smile.dfb86436.svg"
+  }
+]);
